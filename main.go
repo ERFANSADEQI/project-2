222bbaf4f73bfb83dd5b6eaffb2337d0a4af8d3f
ERFAN <sadeqierfan00@gmail.com>
2024-05-10 16:48:01 +0330

    Second commit

--- main.go
+++ main.go
@@ -12,3 +12,29 @@ var wsupgrader = websocket.Upgrader{
 	},
 }
 
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan string)
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	conn, err := wsupgrader.Upgrade(w, r, nil)
+
+    if err!= nil {
+        return
+    }
+
+    defer conn.Close()
+    clients[conn] = true
+    
+	for {
+		_, msg, err := conn.ReadMessage()
+
+		if err!= nil {
+            delete(clients, conn)
+            break
+        }
+
+		broadcast <- string(msg)
+	}
+}
+
+
